6a804f7a020f3603c0314a6a2d942142a8db80c9
bmsrangel <bmsrangel@hotmail.com>
2020-10-25 19:04:19 -0300

    Fixed import in todo_configuration.dart

--- lib/app/config/todo_configuration.dart
+++ lib/app/config/todo_configuration.dart
@@ -1,5 +1,5 @@
 import '../../todo_api.dart';
-import 'database_connection_configuration.dart';
+import './database_connection_configuration.dart';
 
 class TodoConfiguration extends Configuration {
   TodoConfiguration(String fileName) : super.fromFile(File(fileName));
